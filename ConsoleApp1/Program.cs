34a43043f2fbc17c6ebac6719994dcd8ea98e2d8
visualstudio <visualstudio@230-PC25>
2019-03-07 13:48:43 -0500

    commit at 1:48 :

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace lab
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class DelegateExercises
+    {
+        // Student ID:Baldeep Singh Sidhu Student ID:C0726755
+        //Student ID:Simerpreet Singh Student ID:C0726010
+        // Assignment 2
+        //March 7, 2019
+        public delegate void MyDelegate();
+        void Method1()
+        {
+            Console.WriteLine("Method1");
+            Console.ReadLine();
+        }
+        public void Method2()
+        {
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            myDelegate();
         }
     }
 }
