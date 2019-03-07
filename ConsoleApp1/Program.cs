8332c2b51fdd583a0feb36b0ed450562398cbb16
visualstudio <visualstudio@230-PC25>
2019-03-07 15:42:30 -0500

    lab 10 commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,25 +10,48 @@ using System.Threading.Tasks;
 
 namespace Consoleapp1
 {
-    class Program
-    {
-        static void Main(string[] args)
+    
+        class Program
         {
-            DelegateExercises delegateExercises = new DelegateExercises();
-            delegateExercises.Method3();
-            Console.ReadLine();
+            static void Main(string[] args)
+            {
+                DelegateExercises delegateExercises = new DelegateExercises();
+                delegateExercises.Method3();
+                Console.ReadLine();
+
+
+            }
 
 
         }
+        public delegate void MyDelegate();
+        public class DelegateExercises
+
+        {
+
+            void Method1()
+
+            {
+
+                System.Console.WriteLine("Method1");
+
+            }
 
 
+
+            public void Method3()
+
+            {
+
+                MyDelegate myDelegate = new MyDelegate(Method1);
+                myDelegate();
+                System.Console.WriteLine(myDelegate.ToString());
+
+
+            }
+
+
+
+
+        }
     }
-    public delegate void MyDelegate();
-    public class DelegateExercises : MyDelegate
-    {
-
-    }
-
-}
-
-
