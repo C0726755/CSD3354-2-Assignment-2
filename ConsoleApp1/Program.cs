ccfdd1d3af110b571332d962ee62c6360dfea811
visualstudio <visualstudio@230-PC25>
2019-03-07 15:52:32 -0500

    commit at 3:52(lab 11)

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -13,39 +13,28 @@ namespace Consoleapp1
     
         class Program
         {
-            static void Main(string[] args)
-            {
-                DelegateExercises delegateExercises = new DelegateExercises();
-                delegateExercises.Method3();
-                Console.ReadLine();
-
-
-            }
+        static void Main(string[] args)
+        {
+            DelegateExercises delegateExercises = new DelegateExercises();
+            delegateExercises.Method3();
+            Console.ReadLine();
 
 
         }
         public delegate void MyDelegate();
-        public class DelegateExercises
 
+        public class DelegateExercises
         {
 
-            void Method1()
+
+
+
+            void Method3()
 
             {
 
-                System.Console.WriteLine("Method1");
 
-            }
-
-
-
-            public void Method3()
-
-            {
-
-                MyDelegate myDelegate = new MyDelegate(Method1);
-                myDelegate();
-                System.Console.WriteLine(myDelegate.ToString());
+                System.Console.WriteLine(MyDelegate.ToString());
 
 
             }
@@ -55,3 +44,4 @@ namespace Consoleapp1
 
         }
     }
+}
